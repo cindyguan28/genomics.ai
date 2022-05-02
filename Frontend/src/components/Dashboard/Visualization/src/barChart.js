b8844968d177223bf2e2227c7b79375a213a6f57
Xin Guan <64539283+cindyguan28@users.noreply.github.com>
2022-05-02 19:55:43 +0200

    Add files via upload
    fixed axis, title

---
+++ Frontend/src/components/Dashboard/Visualization/src/barChart.js
@@ -0,0 +1,167 @@
+// const { margin } = require("@mui/system");
+import * as d3 from "d3";
+import "./bar.css"
+
+export const addBarPlot = (BarContainer, data) => {
+    const svg = d3.select(BarContainer).append("svg");
+    const margin = 80;
+    const w = 400;
+    const h = 400;
+
+    svg
+        .attr("width", w)
+        .attr("height", h)
+
+    let groupedByCelltypes = new Map();
+    let groupedByBatch = new Map();
+
+    let list = []
+    for (const row in data) {
+        const item = data[row];
+
+        if (groupedByCelltypes.has(item.cell_type)) {
+            list = groupedByCelltypes.get(item.cell_type);
+            list.push(item);
+            groupedByCelltypes.set(item.cell_type, list);
+
+        } else {
+            groupedByCelltypes.set(item.cell_type, [item]);
+        }
+    }
+
+    for (const row in data) {
+        const item = data[row];
+
+        if (groupedByBatch.has(item.batch)) {
+            list = groupedByBatch.get(item.batch);
+            list.push(item);
+            groupedByBatch.set(item.batch, list);
+
+        } else {
+            groupedByBatch.set(item.batch, [item]);
+        }
+    }
+
+    //Scales
+    const info = Array.from(groupedByCelltypes).filter(d => d[0] !== undefined).map(d => [d[0], d[1].length]);
+    const labels = Array.from(groupedByCelltypes.keys()).filter(d => d !== undefined);
+    const xScale = d3.scaleBand().domain(labels).range([margin, w - margin]).padding(0.4);
+    const yScale = d3.scaleLinear().domain([0, data.length]).range([h - margin, 0]);
+
+
+    const g = svg.append("g")
+        .attr("transform", "translate(" + margin + ", 0" + ")");
+
+    //Titel of grafic
+    g.append("text")
+        .attr("transform", "translate(100,0)")
+        .attr("x", 0)
+        .attr("y", margin/2)
+        .attr("font-size", "15px")
+        .text("#Gene per Percentile")
+    //Axes
+    const xAxis = d3.axisBottom(xScale)
+    // .ticks([labels]);
+    const yAxis = d3.axisLeft(yScale).tickFormat(d => d).ticks(3);
+
+
+    g.append("g")
+        .attr("id", "x-axis")
+        .attr("class", "axis")
+        .attr("transform", "translate(0, " + (h - margin) + ")")
+        .call(xAxis)
+        .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end")
+        .style("color", "black")
+    // .attr("stroke", "black");
+
+
+    g.append("g")
+        .attr("id", "y-axis")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + margin + ", 0)")
+        .call(yAxis)
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .style("color", "black")
+
+
+    g
+        .append("g")
+        .attr("id", "bars")
+        .selectAll("rect")
+        .data(info)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", (d) => xScale(d[0]))
+        .attr("y", (d) => yScale(d[1]))
+        .attr("width", xScale.bandwidth())
+        .attr("height", (d) => {
+            console.log(h - yScale(d[1]));
+            return h - yScale(d[1])
+        });
+
+
+    d3.select("#bars")
+        .attr("transform", "translate( 0, " + (-margin) + ")")
+
+
+    // const xScale = d3.scaleBand().range([margin, w - margin]).padding(0.4);
+    // xScale.domain(data.map(function (d) {
+    //   return d.cell_type;
+    // }));
+
+    // const yScale = d3.scaleLinear().range([h - margin,0]);
+    // yScale.domain([0, data.length]);
+
+    // g.append("g")
+    //   .attr("transform", "translate(0," + (h-2*margin) + ")")
+    //   .call(d3.axisBottom(xScale))
+    //   .selectAll("text")
+    //   .attr("transform", "translate(-10,0)rotate(-45)")
+    //   .style("text-anchor", "end")
+
+
+    // g.append("g")
+    //   .call(d3.axisLeft(yScale).tickFormat(function (d) {
+    //     return d;
+    //   })
+    //     .ticks(3))
+    //   .append("text")
+    //   .attr("transform", "rotate(-90)")
+    //   .attr("y", 6)
+    //   .attr("dy", "-5.1em")
+    // // .attr("text-anchor", "end")
+    // // .attr("stroke", "black")
+    // // .text("#All Cells");
+
+    // console.log(groupedByCelltypes);
+
+    // g.selectAll("rect")
+    //   .data(data)
+    //   .enter()
+    //   .append("rect")
+    //   .attr("class", "bar")
+    //   .attr("x", function (d) {
+    //     return xScale(d.cell_type);
+    //   })
+    //   .attr("y", function (d) {
+
+    //     //TODO: switch between cells Type and Batch
+    //     return yScale(getNum(d.cell_type));
+    //   })
+    //   .attr("width", xScale.bandwidth())
+    //   .attr("height", function (d) {
+    //     // return height - yScale(getNum(d.cell_type));
+    //     return yScale(getNum(d.cell_type));
+    //   })
+    //   // .attr("fill", "steelblue")
+    //   .append('title')
+    //   .text(d => getNum(d.cell_type) / data.length)
+
+
+}
+//   );
+// }
